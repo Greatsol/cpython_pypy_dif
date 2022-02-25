244018d38398c0b6d71ed0ddd351ebcd1265b762
Greatsol <ilia2309ilia@gmail.com>
2022-02-25 16:38:24 +0300

    fix: bit_count

--- bit_count/test.py
+++ bit_count/test.py
@@ -1,10 +1,13 @@
+test_numbers = [15, -17, 123435885, 0]
+
+
 def main():
-	pass
-
-
-def aaa():
-    print()
-    main()
+    for number in test_numbers:
+        try:
+            print(f"{number=}")
+            print(f"{number.bit_count()=}")     
+        except AttributeError as err:
+            print(err)
 
 
 if __name__ == "__main__":
